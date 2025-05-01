e5abecc15fcb6a927573ae64fe53150609dcc751
boilakk <orberonsoran@gmail.com>
2025-05-01 18:29:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,9 +7,8 @@ global.socialm = "GitHub: Elite-MD" //ur github or insta name
 global.location = "Port Harcourt, Nigeria" //ur location
 
 //new settings 
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.botname = '𝚮𝚰𝚳' //ur bot name
+global.ownernumber = '2347013259937' //ur owner name
 global.websitex = "https://youtube.com/@elitepro-empire"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
 
@@ -30,7 +29,7 @@ global.typemenu = 'v1'
 global.typereply = 'v4' 
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
+global.welcome = true //welcome/left in groups
 global.anticall = false //bot blocks user when called
 global.autoswview = false //auto status/story view
 global.adminevent = false //show promote/demote message
